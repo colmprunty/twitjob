72ae409d972ff55c4351b946ceae6d8dad70904e
Colm Prunty <colm.prunty@totaljobsgroup.com>
2016-04-15 14:01:49 +0100

    job finder

---
+++ TwitJobConsole/TwitJobConsole/JobFinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TwitJobConsole
+{
+    public class JobFinder
+    {
+        public string FindJob(string jobTitle, string location)
+        {
+            jobTitle = Uri.EscapeDataString(jobTitle);
+            jobTitle = jobTitle.Replace("%20", "+");
+            var url = String.Format("http://www.totaljobs.com/JobSearch/Results.aspx?s=twitterbot&sp=%2FJobSearch%2FResults.aspx&Keywords={0}&LTxt={1}&Radius=0", jobTitle, location);
+
+            return url;
+        }
+    }
+}
--- TwitJobConsole/TwitJobConsole/Program.cs
+++ TwitJobConsole/TwitJobConsole/Program.cs
@@ -8,23 +8,26 @@ namespace TwitJobConsole
         static void Main()
         {
             // Set up your credentials (https://apps.twitter.com)
-            Auth.SetUserCredentials("iVYY8KabChHkgdylQTx9dpvSy", "Y4ZWoutkY2dlkDkHNlPBCOcwDlyZcirE6x6PocwlhBqrBJSk3q", "2897768273-Wn0wb4ReNUt19dMtyhEXkpQDhmo42AfBzflzSWk", "AF2cMV4H42EtJuSjJNd4SGOQPr2RzH20b2oespGE7DxpS");
+            //Auth.SetUserCredentials("iVYY8KabChHkgdylQTx9dpvSy", "Y4ZWoutkY2dlkDkHNlPBCOcwDlyZcirE6x6PocwlhBqrBJSk3q", "2897768273-Wn0wb4ReNUt19dMtyhEXkpQDhmo42AfBzflzSWk", "AF2cMV4H42EtJuSjJNd4SGOQPr2RzH20b2oespGE7DxpS");
 
-            var stream = Stream.CreateUserStream();
-            stream.TweetCreatedByAnyoneButMe += (sender, args) =>
-            {
-                if (args.Tweet.Text.StartsWith("@jkim_project"))
-                {
-                    Console.WriteLine("Here's the thing" + args.Tweet);
-                    Tweet.PublishTweetInReplyTo("@" + args.Tweet.CreatedBy.ScreenName + " Go away ", args.Tweet);
-                }
-                else
-                    Console.WriteLine("Spam!");
+            //var stream = Stream.CreateUserStream();
+            //stream.TweetCreatedByAnyoneButMe += (sender, args) =>
+            //{
+            //    if (args.Tweet.Text.StartsWith("@jkim_project"))
+            //    {
+            //        Console.WriteLine("From: @" + args.Tweet.CreatedBy.ScreenName + " Content: " + args.Tweet.Text);
+            //        Tweet.PublishTweetInReplyTo("@" + args.Tweet.CreatedBy.ScreenName + " Go away ", args.Tweet);
+            //    }
+            //    else
+            //        Console.WriteLine("From: @" + args.Tweet.CreatedBy.ScreenName + " Content: " + args.Tweet.Text);
+            //};
 
-                //Tweet.PublishTweetInReplyTo("@" + args.Tweet.CreatedBy.ScreenName + " Go away", args.Tweet);
-            };
+            //stream.StartStream();
 
-            stream.StartStream();
+            var finder = new JobFinder();
+            var jobLink = finder.FindJob("C# developer", "london");
+
+            Console.WriteLine(jobLink);
 
             Console.ReadLine();
         }

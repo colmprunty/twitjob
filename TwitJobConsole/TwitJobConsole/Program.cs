e9dd900b564d7254414a7465615997f276766ccd
Colm Prunty <colm.prunty@totaljobsgroup.com>
2016-04-15 14:52:18 +0100

    Hey it works

--- TwitJobConsole/TwitJobConsole/Program.cs
+++ TwitJobConsole/TwitJobConsole/Program.cs
@@ -7,29 +7,33 @@ namespace TwitJobConsole
     {
         static void Main()
         {
-            // Set up your credentials (https://apps.twitter.com)
-            //Auth.SetUserCredentials("iVYY8KabChHkgdylQTx9dpvSy", "Y4ZWoutkY2dlkDkHNlPBCOcwDlyZcirE6x6PocwlhBqrBJSk3q", "2897768273-Wn0wb4ReNUt19dMtyhEXkpQDhmo42AfBzflzSWk", "AF2cMV4H42EtJuSjJNd4SGOQPr2RzH20b2oespGE7DxpS");
-
-            //var stream = Stream.CreateUserStream();
-            //stream.TweetCreatedByAnyoneButMe += (sender, args) =>
-            //{
-            //    if (args.Tweet.Text.StartsWith("@jkim_project"))
-            //    {
-            //        Console.WriteLine("From: @" + args.Tweet.CreatedBy.ScreenName + " Content: " + args.Tweet.Text);
-            //        Tweet.PublishTweetInReplyTo("@" + args.Tweet.CreatedBy.ScreenName + " Go away ", args.Tweet);
-            //    }
-            //    else
-            //        Console.WriteLine("From: @" + args.Tweet.CreatedBy.ScreenName + " Content: " + args.Tweet.Text);
-            //};
-
-            //stream.StartStream();
-
             var finder = new JobFinder();
-            var jobLink = finder.FindJob("C# developer", "london");
+            var parser = new TweetParser();
+            
+            Auth.SetUserCredentials("iVYY8KabChHkgdylQTx9dpvSy", "Y4ZWoutkY2dlkDkHNlPBCOcwDlyZcirE6x6PocwlhBqrBJSk3q", "2897768273-Wn0wb4ReNUt19dMtyhEXkpQDhmo42AfBzflzSWk", "AF2cMV4H42EtJuSjJNd4SGOQPr2RzH20b2oespGE7DxpS");
 
-            Console.WriteLine(jobLink);
 
+            var stream = Stream.CreateUserStream();
+            stream.TweetCreatedByAnyoneButMe += (sender, args) =>
+            {
+                if (args.Tweet.Text.StartsWith("@jkim_project"))
+                {
+                    var tweetText = TrimOffTwitterUsername(args.Tweet.Text, "@jkim_project ");
+                    var tweetInfo = parser.ParseTweet(tweetText);
+                    var jobUrl = finder.FindJob(tweetInfo.JobTitle, tweetInfo.Location);
+                    Tweet.PublishTweetInReplyTo("@" + args.Tweet.CreatedBy.ScreenName + " Here's a decent job: " + jobUrl, args.Tweet);
+                }
+                else
+                    Console.WriteLine("Just spam");
+            };
+
+            stream.StartStream();
             Console.ReadLine();
+        }
+
+        private static string TrimOffTwitterUsername(string text, string user)
+        {
+            return text.Substring(user.Length);
         }
     }
 }
---
+++ TwitJobConsole/TwitJobConsole/TweetParser.cs
@@ -0,0 +1,47 @@
+﻿namespace TwitJobConsole
+{
+    public class TweetParser
+    {
+        public TweetInfo ParseTweet(string tweetText)
+        {
+            if (!tweetText.Contains(" jobs"))
+                return JobTitleOnly(tweetText);
+
+            if (tweetText.Contains(" jobs") && !tweetText.Contains(" in "))
+                return IncludingKeyword(tweetText, " jobs");
+
+            if (tweetText.Contains(" in "))
+                return IncludingKeyword(tweetText, " in ");
+
+            return new TweetInfo();
+        }
+
+        private TweetInfo JobTitleOnly(string tweetText)
+        {
+            return new TweetInfo
+            {
+                JobTitle = tweetText,
+                Location = ""
+            };
+        }
+
+        private TweetInfo IncludingKeyword(string tweetText, string splitOn)
+        {
+            var textIndex = tweetText.IndexOf(splitOn);
+            var jobTitle = tweetText.Substring(0, textIndex);
+            var location = splitOn == " in " ? tweetText.Substring(textIndex + 4) : "";
+
+            return new TweetInfo
+            {
+                JobTitle = jobTitle,
+                Location = location
+            };
+        }
+    }
+
+    public class TweetInfo
+    {
+        public string JobTitle { get; set; }
+        public string Location { get; set; }
+    }
+}

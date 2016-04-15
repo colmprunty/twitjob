e8c973df5e602b7d505772596e40214f241b36f3
Colm Prunty <colm.prunty@totaljobsgroup.com>
2016-04-15 11:29:39 +0100

    streaming!

--- TwitJobConsole/TwitJobConsole/Program.cs
+++ TwitJobConsole/TwitJobConsole/Program.cs
@@ -7,21 +7,18 @@ namespace TwitJobConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Set up your credentials (https://apps.twitter.com)
             Auth.SetUserCredentials("iVYY8KabChHkgdylQTx9dpvSy", "Y4ZWoutkY2dlkDkHNlPBCOcwDlyZcirE6x6PocwlhBqrBJSk3q", "2897768273-Wn0wb4ReNUt19dMtyhEXkpQDhmo42AfBzflzSWk", "AF2cMV4H42EtJuSjJNd4SGOQPr2RzH20b2oespGE7DxpS");
 
-            var mentionsTimelineParameters = new MentionsTimelineParameters();
-            var tweets = Timeline.GetMentionsTimeline(mentionsTimelineParameters);
+            var stream = Stream.CreateUserStream();
+            stream.TweetCreatedByAnyone += (sender, args) =>
+            {
+                Console.WriteLine("Here's the thing" + args.Tweet);
+            };
 
-            var mention = tweets.First();
-            Console.WriteLine(mention.Text);
-
-            var theUser = mention.CreatedBy;
-
-            var text = "@" + theUser.ScreenName + " here's a job for you";
-            Tweet.PublishTweetInReplyTo(text, mention.Id);
+            stream.StartStream();
 
             Console.ReadLine();
         }

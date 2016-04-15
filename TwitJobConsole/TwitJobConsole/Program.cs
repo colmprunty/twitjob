edabe38283279dc7f85b6b08cbe6a43bd389e1ad
Colm Prunty <colm.prunty@totaljobsgroup.com>
2016-04-15 11:21:52 +0100

    replying to texts

--- TwitJobConsole/TwitJobConsole/Program.cs
+++ TwitJobConsole/TwitJobConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿using Tweetinvi;
+﻿using System;
+using System.Linq;
+using Tweetinvi;
+using Tweetinvi.Core.Parameters;
 
 namespace TwitJobConsole
 {
@@ -9,8 +12,18 @@ namespace TwitJobConsole
             // Set up your credentials (https://apps.twitter.com)
             Auth.SetUserCredentials("iVYY8KabChHkgdylQTx9dpvSy", "Y4ZWoutkY2dlkDkHNlPBCOcwDlyZcirE6x6PocwlhBqrBJSk3q", "2897768273-Wn0wb4ReNUt19dMtyhEXkpQDhmo42AfBzflzSWk", "AF2cMV4H42EtJuSjJNd4SGOQPr2RzH20b2oespGE7DxpS");
 
-            // Publish the Tweet "Hello World" on your Timeline
-            Tweet.PublishTweet("Look, a job!");
+            var mentionsTimelineParameters = new MentionsTimelineParameters();
+            var tweets = Timeline.GetMentionsTimeline(mentionsTimelineParameters);
+
+            var mention = tweets.First();
+            Console.WriteLine(mention.Text);
+
+            var theUser = mention.CreatedBy;
+
+            var text = "@" + theUser.ScreenName + " here's a job for you";
+            Tweet.PublishTweetInReplyTo(text, mention.Id);
+
+            Console.ReadLine();
         }
     }
 }
